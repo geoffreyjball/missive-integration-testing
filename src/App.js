193d2cb8feed11a29eeb51bd68860adfa5deca69
Geoff Ball <geoffreyjball@gmail.com>
2020-09-14 16:00:50 -0600

    Add a Missive class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <header className='App-header'>
-          <p>Playing around with Missive integrations.</p>
+          <p class='text-large'>Playing around with Missive integrations.</p>
         </header>
       </div>
     );
