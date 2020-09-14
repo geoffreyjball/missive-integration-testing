8f9556e5bc04e7ecb87433831814c28d371f2359
Geoff Ball <geoffreyjball@gmail.com>
2020-09-14 16:52:36 -0600

    Get API connected

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 class App extends Component {
   render() {
+    const getRando = async () => {
+      try {
+        const res = await axios.get('https://randomuser.me/api/');
+        // const good = JSON.stringify(res);
+        console.log(res.data.results[0].gender);
+        const gender = res.data.results[0].gender;
+        return gender;
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
     return (
       <div className='App'>
         <header className='App-header'>
-          <p class='text-large'>Playing around with Missive integrations.</p>
+          <p className='text-large'>Playing around with Missive integrations.</p>
+          <p>{getRando()}</p>
         </header>
       </div>
     );
