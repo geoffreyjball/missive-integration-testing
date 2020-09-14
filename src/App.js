1af4f299bdd91b7b3509bfdb2007d8e3290ac32f
Geoff Ball <geoffreyjball@gmail.com>
2020-09-14 15:51:49 -0600

    Remove reference to Lambda demo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import React, { Component } from 'react';
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { loading: false, msg: null };
-  }
-}
-
 class App extends Component {
   render() {
     return (
